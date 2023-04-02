d9fd981d9b9c197cd7390062a9813a4fa36bf54d
jong-k <kjhan710@naver.com>
2023-04-02 22:31:52 +0900

    refactor: 불필요한 계산 제거 및 컬러 변환

--- core3/eyes-on-cursor/app.js
+++ core3/eyes-on-cursor/app.js
@@ -1,22 +1,23 @@
+const mainImage = document.getElementById("main");
+// 메인 이미지를 둘러싼 직사각형 관련 정보를 가짐
+const rect = mainImage.getBoundingClientRect();
+
+// 메인 이미지의 정중앙 좌표
+const mainImageX = rect.left + rect.width / 2;
+const mainImageY = rect.top + rect.height / 2;
+
 document.addEventListener("mousemove", (e) => {
   // 마우스 커서 좌표
   const cursorX = e.clientX;
   const cursorY = e.clientY;
 
-  const mainImage = document.getElementById("main");
-  // 메인 이미지를 둘러싼 직사각형 관련 정보를 가짐
-  const rect = mainImage.getBoundingClientRect();
-
-  // 메인 이미지의 정중앙 좌표
-  const mainImageX = rect.left + rect.width / 2;
-  const mainImageY = rect.top + rect.height / 2;
-
   const angleDeg = getAngleDeg(mainImageX, mainImageY, cursorX, cursorY);
 
   const eyes = document.querySelectorAll(".eye");
-  eyes.forEach(
-    (eye) => (eye.style.transform = `rotate(${22.5 + angleDeg}deg)`),
-  );
+  eyes.forEach((eye) => {
+    eye.style.transform = `rotate(${22.5 + angleDeg}deg)`;
+    mainImage.style.filter = `hue-rotate(${angleDeg}deg)`;
+  });
 });
 
 // x1, y1 : 메인 이미지 정중앙의 좌표

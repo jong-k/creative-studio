6c11513fb15ad9b726401b5e54589ce387418d74
jhkim-hdxwill <jhkim00@iwillmed.com>
2023-09-06 11:52:19 +0900

    chore: role 데이터 추가

---
+++ story-component/src/components/Dropdown/data.ts
@@ -0,0 +1,14 @@
+export const ROLES = [
+  {
+    name: "의사",
+    value: "doctor",
+  },
+  {
+    name: "기공사",
+    value: "technician",
+  },
+  {
+    name: "환자",
+    value: "patient",
+  },
+];
